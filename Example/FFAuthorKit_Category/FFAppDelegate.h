d8bbe4d94650f1149d3df5f5414d6e73d502994d
陈文杰 <393304520@qq.com>
2019-02-18 10:14:40 +0800

    Initial commit

---
+++ Example/FFAuthorKit_Category/FFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FFAppDelegate.h
+//  FFAuthorKit_Category
+//
+//  Created by stevenJiechen on 02/18/2019.
+//  Copyright (c) 2019 stevenJiechen. All rights reserved.
+//
+
+@import UIKit;
+
+@interface FFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/FFAuthorKit_Category/FFViewController.h
@@ -0,0 +1,13 @@
+//
+//  FFViewController.h
+//  FFAuthorKit_Category
+//
+//  Created by stevenJiechen on 02/18/2019.
+//  Copyright (c) 2019 stevenJiechen. All rights reserved.
+//
+
+@import UIKit;
+
+@interface FFViewController : UIViewController
+
+@end
